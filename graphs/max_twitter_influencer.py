70a08f136e26ff815e2e689950bc923db60d6ec9
Madan Mohan Mulpuri <mmulpuri@vmware.com>
2023-03-13 21:39:13 -0700

    graphs bug fix

--- graphs/max_twitter_influencer.py
+++ graphs/max_twitter_influencer.py
@@ -1,5 +1,5 @@
 
-def find_followers(key: str, graph_edges: {}, visited_vertex_list: {}) -> int:
+def find_followers(key: str, graph_edges: dict, visited_vertex_list: dict) -> int:
    if key in graph_edges:
       if key not in visited_vertex_list:
          visited_vertex_list[key] = 1
@@ -12,18 +12,24 @@ def find_followers(key: str, graph_edges: {}, visited_vertex_list: {}) -> int:
       return 1
    
 
-def find_max_influencer(graph_edges: {}) -> None:
+def find_max_influencer(graph_edges: dict) -> str:
+   max_follwers = 0
+   max_influencer = ""
    for key in graph_edges.keys():
-      max_influence = find_followers(key, graph_edges, {})
-      print(f"max influence of {key} is {max_influence}")
+      follwers = find_followers(key, graph_edges, {})
+      print(f"number of followers for {key} is {follwers}")
+      if max_follwers < follwers:
+         max_follwers = follwers
+         max_influencer = key
+   return max_influencer
 
-def create_graph(tw_f: []) -> {}:
+def create_graph(tw_f: list) -> dict:
    graph_edges = {} 
    for tup in tw_f:
       # using tup[1] as key instead of tup[0]
-      # so, instead of "stuart follows marty"
-      # the graph_edges edge indicates "marty is followed by stuart"
-      # this helps in finding followers of stuart and recursively followers of follwer
+      # so, instead of storing "stuart follows marty"
+      # the graph_edges indicates "marty is followed by stuart"
+      # this helps in recursively finding followers of stuart and their followers
       if tup[1] in graph_edges:
          graph_edges[tup[1]].append(tup[0])
       else:
@@ -33,19 +39,20 @@ def create_graph(tw_f: []) -> {}:
 if __name__ == "__main__":
    # list of tuples
    twitter_followers = [
-                        ('stuart',  'marty'),   # stuart follows marty
-                        ('helene',  'elmer'),   # helene follows elmer
-                        ('donald',  'marty'),
-                        ('bruce',   'elmer'),
-                        ('donald',  'elmer'),
-                        ('donald',  'stuart'),
-                        ('stuart',  'mehran'),
-                        ('mehran',  'donald'),
-                        ('reid',    'elmer'),
-                        ('marty',   'mehran'),
+                        ('Stuart',  'Marty'),   # stuart follows marty
+                        ('Helene',  'Elmer'),   # helene follows elmer
+                        ('Donald',  'Marty'),
+                        ('Bruce',   'Elmer'),
+                        ('Donald',  'Elmer'),
+                        ('Donald',  'Stuart'),
+                        ('Stuart',  'Mehran'),
+                        ('Mehran',  'Donald'),
+                        ('Reid',    'Elmer'),
+                        ('Marty',   'Mehran'),
                         ]
     
    graph_edges = create_graph(twitter_followers)
    print(f"Graph of twitter followers {graph_edges}")
 
-   find_max_influencer(graph_edges)
+   max_influencer = find_max_influencer(graph_edges)
+   print(f"\nMAX Influencer is {max_influencer}\n")
